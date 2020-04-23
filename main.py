7f726fd12420dcf2a9307b61d7876540bea0bab7
Dominik Sánchez <4673828+dsancheznet@users.noreply.github.com>
2020-04-23 13:21:34 +0100

    UPDATE: Error correction

--- main.py
+++ main.py
@@ -27,7 +27,6 @@ class SymbolExtension(Extension):
     def __init__(self):
         super(SymbolExtension, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
-        self.subscribe(ItemEnterEvent, ItemEnterEventListener())
 
 
 class KeywordQueryEventListener(EventListener):
